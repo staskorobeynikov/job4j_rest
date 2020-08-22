a3afef3059125c9c3c914cb3a94e29b2e948e0c1
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-08-22 18:29:31 +0300

    0. RESTFull. Описание архитектуры[#6884]

---
+++ src/main/java/ru/job4j/auth/AuthApp.java
@@ -0,0 +1,24 @@
+package ru.job4j.auth;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class AuthApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApp.class, args);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
+}
